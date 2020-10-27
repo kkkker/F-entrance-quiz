78b4cdaced7df018f9e409b13adb7af4ce0bd5b9
kker <907765631@qq.com>
2020-10-27 21:06:22 +0800

    [Mamingjun] refactor: implement title of group student list

--- src/App/App.js
+++ src/App/App.js
@@ -35,6 +35,12 @@ class App extends Component {
     return (
       <div data-testid="app" className="App">
         <div className="list-item">
+          <div className="group-title">
+            <h2>分组列表</h2>
+            <button type="button">分组学员</button>
+          </div>
+        </div>
+        <div className="list-item">
           <h2>学员列表</h2>
           <div className="students">
             {this.state.students.map((student) => {

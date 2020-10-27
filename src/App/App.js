a2573bf0afe5707ce7944a28459d866360ab9f0f
kker <907765631@qq.com>
2020-10-27 20:03:14 +0800

    [Mamingjun] feat: implement get student from back end

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {};
+
+  componentDidMount = () => {
+    this.getStudents();
+  };
+
+  getStudents = () => {
+    const url = 'http://localhost:8080/students';
+    fetch(url, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'GET',
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => console.log(data));
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <div>
+          <h2>学员列表</h2>
+        </div>
       </div>
     );
   }
